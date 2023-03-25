5e99c37962bb9d10049664c4c2444c7bc8089716
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-03-25 15:57:17 +0900

    feat: 서식 변경
    feat: 서식 변경

--- src/components/SurveyList/SurveyList.styles.tsx
+++ src/components/SurveyList/SurveyList.styles.tsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 const ListContainer = styled.ul`
-
+  margin-top: 2rem;
 `;
 
 const CardContainer = styled.li`
-
+  margin: 1rem;  
+  padding: 2rem;
+  background-color: #fff;
+  border-radius: 1rem;
 `;
 
 const SurveyTitle = styled.h2`
-
+  line-height: 2rem;
 `;
 
 const SurveyContent = styled.div`
